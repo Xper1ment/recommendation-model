afe9cd95d7b31811121fcbc6cc7598aeecae00ca
real-player <31935674+real-player@users.noreply.github.com>
2020-04-26 14:31:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def get_recommendations(title, cosine_sim):
 
 @app.route('/recommend/',methods=['GET'])
 
-def recommend(x,y):
+def recommend():
     title = request.args.get("title", None)
     result = get_recommendations(title,cosine_sim2)
     return result.to_json()
