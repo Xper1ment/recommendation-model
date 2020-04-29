6bfb823d4e966e7bf8e6baa77ab728404813879e
real-player <gdgsonu@gmail.com>
2020-04-29 14:54:02 +0530

    now api gives web links too

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle ,json
 from flask import Flask,jsonify,request
 from flask_cors import CORS
 import json
@@ -20,6 +20,13 @@ df2 = df2.reset_index()
 cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
 indices = pd.Series(df2.index, index=df2['title'])
 
+def mergeDict(dict1, dict2):
+    ''' Merge dictionaries and keep values of common keys in list'''
+    dict3 = {**dict1, **dict2}
+    for key, value in dict3.items():
+        if key in dict1 and key in dict2:
+            dict3[key] = [value , dict1[key]]   
+    return dict3
     
 def get_recommendations(title, cosine_sim):
     # Get the index of the movie that matches the title
@@ -36,17 +43,22 @@ def get_recommendations(title, cosine_sim):
 
     # Get the movie indices
     movie_indices = [i[0] for i in sim_scores]
-
+    
     # Return the top 10 most similar movies
-    return df2['title'].iloc[movie_indices]
-
+    dict1 =  df2['title'].iloc[movie_indices].to_dict()
+    dict2 = df2['homepage'].iloc[movie_indices].to_dict()
+    dict3 = mergeDict(dict2,dict1)
+    keys_values = dict3.items()
+    new_d = {str(key): value for key, value in keys_values}
+    result = json.dumps(new_d) 
+    return result
 
 @app.route('/recommend/',methods=['GET'])
 
 def recommend():
     title = request.args.get("title", None)
     result = get_recommendations(title,cosine_sim2)
-    return result.to_json()
+    return result
 
 @app.route('/')
 
