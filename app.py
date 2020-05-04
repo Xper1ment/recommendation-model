2ac0169368c85008cdcc5bf341c24285a891e298
real-player <gdgsonu@gmail.com>
2020-05-04 13:43:41 +0530

    handling crashing issues

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import pickle ,json,math
 from flask import Flask,jsonify,request
-from book import recomm_books
 from flask_cors import CORS
 import json
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity,linear_kernel
 
 app = Flask(__name__)
 CORS(app)
@@ -28,7 +27,6 @@ def mergeDict(dict1, dict2):
         if key in dict1 and key in dict2:
             dict3[key] = {'name': value , 'website': dict1[key]}   
     return dict3
-
 
 def get_recommendations(title, cosine_sim):
     # Get the index of the movie that matches the title
@@ -57,6 +55,35 @@ def get_recommendations(title, cosine_sim):
     result = {str(key): value for key, value in keys_values}
     #result = json.dumps(new_d) 
     return result
+
+with open('./data/book_model.pkl', 'rb') as f:
+    tfidf_matrix_corpus = pickle.load(f)
+
+with open('./data/books.pkl','rb') as f:
+    books = pickle.load(f)
+
+cosine_sim_corpus = linear_kernel(tfidf_matrix_corpus, tfidf_matrix_corpus)
+
+
+# Build a 1-dimensional array with book titles
+titles = books['title']
+url = books['image_url']
+indices = pd.Series(books.index, index=books['title'])
+
+# Function that get book recommendations based on the cosine similarity score of books tags
+def recomm_books(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim_corpus[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:21]
+    book_indices = [i[0] for i in sim_scores]
+    urls = url.iloc[book_indices].to_dict()
+    name = titles.iloc[book_indices].to_dict()
+    ans = mergeDict(urls,name)
+    keys_values = ans.items()
+    result = {str(key): value for key, value in keys_values}
+    return result
+
 
 @app.route('/recommend/',methods=['GET'])
 
